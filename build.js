a5c9920cdf2cd7c662aa21793651ea0f98f1f38a
Eirene <sorokina333@gmail.com>
2022-04-13 20:10:36 +0300

    feat: update UI for main page

--- build.js
+++
@@ -1,7 +0,0 @@
-var { readdirSync, readFileSync } = require('fs');
-
-const filenames = readdirSync(__dirname + 'src');
-filenames.forEach(filename => {
-  const fileContent = readFileSync(__dirname + filename, 'utf-8');
-  console.log(fileContent);
-});
