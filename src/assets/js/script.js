3bc8cc0161d0d563f8a8ae3f3a3fa39f941df141
Anton Shubin <2spy4x@gmail.com>
2022-05-20 20:19:06 +0300

    feat: add subscribe form for the blog (#6)

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,7 @@
+function onInit(fn) {
+  window.addEventListener('load', fn);
+}
+
 const menuItemClasses = {
   active: ['bg-orange-500', 'text-white'],
   default: ['text-gray-300', 'hover:bg-gray-700', 'hover:text-white'],
@@ -51,17 +55,99 @@ setTimeout(() => {
     .forEach(el => el.setAttribute('href', 'mai' + 'lto' + ':' + '2sp' + 'y' + 4 + 'x+ws@' + 'gm' + 'ail.' + 'com'));
 }, 3000);
 
-window.addEventListener('load', () => {
-  const script = document.createElement('script');
-  script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-R9W8GJC3FZ');
-  script.setAttribute('defer', '');
-  script.onload = () => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'G-R9W8GJC3FZ');
-  };
-  document.body.appendChild(script);
+function addScript(url, onLoadFn) {
+  onInit(() => {
+    const script = document.createElement('script');
+    script.setAttribute('src', url);
+    script.setAttribute('defer', '');
+    script.onload = onLoadFn;
+    document.body.appendChild(script);
+  });
+}
+
+// Add Google Analytics
+addScript('https://www.googletagmanager.com/gtag/js?id=G-R9W8GJC3FZ', () => {
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+  gtag('config', 'G-R9W8GJC3FZ');
 });
+
+// Add Google Recaptcha v3
+const RECAPTCHA_PUBLIC_KEY = '6LcCmv0fAAAAAJIKAhoF2JfeYJYMhKPfCa1J0UVV';
+addScript(`https://www.google.com/recaptcha/api.js?render=${RECAPTCHA_PUBLIC_KEY}`);
+
+function isEmailValid(email) {
+  const regex =
+    /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+  return regex.test(email);
+}
+
+// Blog subscribe form
+onInit(() => document.querySelector('#subscribe-email-button').addEventListener('click', subscribeEmail));
+
+function subscribeEmail(event) {
+  event.preventDefault();
+  grecaptcha.ready(async () => {
+    const recaptchaToken = await grecaptcha.execute(RECAPTCHA_PUBLIC_KEY, { action: 'subscribeEmail' });
+    const email = document.querySelector('#subscribe-email-input').value.trim();
+    const subscribeEmailValidationEl = document.querySelector('#subscribe-email-validation');
+    if (isEmailValid(email)) {
+      subscribeEmailValidationEl.style.display = 'none';
+    } else {
+      subscribeEmailValidationEl.style.display = 'block';
+      return;
+    }
+    updateButtonLoadingState('loading');
+    const response = await fetch('/api/subscribe-email', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, recaptchaToken }),
+    });
+    const json = await response.json();
+    if (response.status === 200) {
+      updateButtonLoadingState('success');
+    } else {
+      updateButtonLoadingState('error');
+      console.error(json);
+    }
+  });
+}
+
+function updateButtonLoadingState(state) {
+  switch (state) {
+    case 'loading': {
+      const button = document.querySelector('#subscribe-email-button');
+      const loading = button.querySelector('.loading');
+      loading.classList.remove('hidden', 'loading--success', 'loading--error');
+      const span = button.querySelector('span');
+      span.classList.add('hidden');
+      break;
+    }
+    case 'success': {
+      const button = document.querySelector('#subscribe-email-button');
+      const loading = button.querySelector('.loading');
+      loading.classList.add('loading--success');
+      const span = button.querySelector('span');
+      span.classList.remove('hidden');
+      span.innerText = 'Done';
+      break;
+    }
+    case 'error': {
+      const button = document.querySelector('#subscribe-email-button');
+      const loading = button.querySelector('.loading');
+      loading.classList.add('loading--error');
+      const span = button.querySelector('span');
+      span.classList.remove('hidden');
+      span.innerText = 'Error';
+      break;
+    }
+    default: {
+      console.error(`Function "updateButtonLoadingState(state)" was invoked with incorrect argument "${state}".`);
+    }
+  }
+}
