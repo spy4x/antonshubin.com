fd4e5a9a1f034bd17e867b0ebfd6586496dcc17b
Anton Shubin <2spy4x@gmail.com>
2022-05-30 18:29:23 +0300

    env: update dependencies

--- build-blog.js
+++ build-blog.js
@@ -1,12 +1,12 @@
 const pug = require('pug');
 const pugJSON = require('./src/pug.json');
 const fs = require('fs');
-const marked = require('marked');
+const { marked } = require('marked');
 
 const blogArticleTemplate = pug.compileFile('./src/partials/blog-item.pug', { pretty: true });
 
 for (let article of pugJSON.data.blogArticles) {
-  article.content = marked(article.mdContent);
+  article.content = marked.parse(article.mdContent);
   article.url = '/blog/' + article.slug;
   const html = blogArticleTemplate(article);
   fs.writeFileSync(`dist/blog/${article.slug}.html`, html);
