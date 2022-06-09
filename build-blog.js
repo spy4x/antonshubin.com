0d55e346bb39b2f7d0e2ce8744816fca1abf8c54
Anton Shubin <2spy4x@gmail.com>
2022-06-10 00:04:01 +0300

    blog: add post "My journey from an office job to personal projects"

--- build-blog.js
+++ build-blog.js
@@ -5,8 +5,22 @@ const { marked } = require('marked');
 
 const blogArticleTemplate = pug.compileFile('./src/partials/blog-item.pug', { pretty: true });
 
+// lazy loading images
+marked.use({
+  extensions: [
+    {
+      name: 'image',
+      level: 'inline',
+      renderer(token) {
+        const html = this.parser.renderer.image(token.href, token.title, token.text);
+        return html.replace(/^<img /, '<img loading="lazy" ');
+      },
+    },
+  ],
+});
+
 for (let article of pugJSON.data.blogArticles) {
-  article.content = marked.parse(article.mdContent);
+  article.content = marked.parse(fs.readFileSync(`src/views/blog/posts/${article.slug}.md`).toString());
   article.url = '/blog/' + article.slug;
   const html = blogArticleTemplate(article);
   fs.writeFileSync(`dist/blog/${article.slug}.html`, html);
